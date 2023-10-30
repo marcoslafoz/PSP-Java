2ad5edaf36385c107dc0947543056eb695f3aae9
Marcos Lafoz <marcos.lafoz96@gmail.com>
2023-10-30 19:34:57 +0100

    feat: :heavy_plus_sign: Ejercicios Threads

---
+++ EjerciciosPSP/src/unidad2/ejercicio1x01/Fibonacci.java
@@ -0,0 +1,23 @@
+package unidad2.ejercicio1x01;
+
+public class Fibonacci {
+    public static void FibonacciSequence(int n) {
+        if (n <= 0) {
+            System.out.println("El número debe ser mayor que cero.");
+            return;
+        }
+
+        int a = 0, b = 1;
+        System.out.print("Secuencia de Fibonacci hasta " + n + ": ");
+        System.out.print(a);
+
+        while (b <= n) {
+            System.out.print(", " + b);
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        System.out.println();
+    }
+}
--- EjerciciosPSP/src/unidad2/ejercicio1x01/Main.java
+++ EjerciciosPSP/src/unidad2/ejercicio1x01/Main.java
@@ -1,9 +1,38 @@
 package unidad2.ejercicio1x01;
 
-public class Main {
-	public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Main { 
+	public static void main(String [] args) throws InterruptedException { 
 		
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 System.out.println("Introduce hasta que numero desea hacer la sucesión de Fibonacci");
+		 int nValue = scanner.nextInt();
 		
+		HiloFibonacci hilo1 = new HiloFibonacci("HiloFibonacci1", nValue);
+		hilo1.start();
 		
+		System.out.println("Fin del programa principal");
 	}
+
+}
+
+class HiloFibonacci extends Thread{
+	private String nombre;
+	private int nValue;
+	
+	HiloFibonacci(String nombre, int nValue){
+		this.nombre = nombre;
+		this.nValue = nValue;
+	}
+
+	@Override
+	public void run() {
+		Fibonacci.FibonacciSequence(nValue);
+	}
+	
+    
+	
+
 }
